ccacb2eafe3bd9bccf4f36dabf18af69adf0d587
Ulan Seitkaliyev <67548058+Inverseit@users.noreply.github.com>
2022-09-22 14:20:36 +0300

    fix jest exit error

--- tests/integration/docs.test.js
+++ tests/integration/docs.test.js
@@ -7,8 +7,12 @@ describe("Auth routes", () => {
   describe("GET /v1/docs", () => {
     test("should return 404 when running in production", async () => {
       config.env = "production";
-      await request(app).get("/v1/docs").send().expect(httpStatus.NOT_FOUND);
+      const res = await request(app)
+        .get("/v1/docs")
+        .send()
+        .expect(httpStatus.NOT_FOUND);
       config.env = process.env.NODE_ENV;
+      return res;
     });
   });
 });
